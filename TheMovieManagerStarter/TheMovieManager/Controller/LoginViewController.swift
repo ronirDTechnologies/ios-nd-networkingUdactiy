50436b095fdea14aeafd0e7a5e9fdc9a2606bf1c
RoniUdacity <roni@dinatechnologies.com>
2019-06-28 23:10:55 -0400

    Handle differnent network calls
    RR: 06-29-2019
    Handle network calls to perform the following tasks:
    1. create session id
    2. login
    3. Web Auth
    4.logout

--- TheMovieManagerStarter/TheMovieManager/Controller/LoginViewController.swift
+++ TheMovieManagerStarter/TheMovieManager/Controller/LoginViewController.swift
@@ -18,8 +18,22 @@ class LoginViewController: UIViewController {
     func getTokenHelper(success: Bool, error: Error?) -> Void {
         if success {
             print(TMDBClient.Auth.requestToken)
+            DispatchQueue.main.async {
+                TMDBClient.login(userName: self.emailTextField.text ?? "", password: self.passwordTextField.text ?? "", completion: self.handleLoginResponse(succes:Error:))
+            }
+            TMDBClient.getRequestSessionId(completion: handleSessionResponse(success:error:))
         }
         
+    }
+    func handleSessionResponse(success: Bool, error: Error?){
+        if success{
+            DispatchQueue.main.async {
+                self.performSegue(withIdentifier: "completeLogin", sender: nil)
+            }
+        }
+    }
+    func handleLoginResponse(succes: Bool, Error: Error?){
+        print(TMDBClient.Auth.requestToken)
     }
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -33,7 +47,13 @@ class LoginViewController: UIViewController {
     }
     
     @IBAction func loginViaWebsiteTapped() {
-        performSegue(withIdentifier: "completeLogin", sender: nil)
+        TMDBClient.getRequestToken{ (success,error) in
+            if success{
+                DispatchQueue.main.async {
+                  UIApplication.shared.open(TMDBClient.Endpoints.webAuth.url, options: [:], completionHandler: nil)
+                    self.performSegue(withIdentifier: "completeLogin", sender: nil)
+                }
+            }
+        }
     }
-    
 }
--- TheMovieManagerStarter/TheMovieManager/Model/TMDB Client/TMDBClient.swift
+++ TheMovieManagerStarter/TheMovieManager/Model/TMDB Client/TMDBClient.swift
@@ -24,11 +24,19 @@ class TMDBClient {
         
         case getWatchlist
         case getRequestToken
+        case createSessionId
+        case login
+        case webAuth
+        case logOut
         
         var stringValue: String {
             switch self {
             case .getWatchlist: return Endpoints.base + "/account/\(Auth.accountId)/watchlist/movies" + Endpoints.apiKeyParam + "&session_id=\(Auth.sessionId)"
             case .getRequestToken: return Endpoints.base + "/authentication/token/new" + Endpoints.apiKeyParam
+            case .createSessionId: return Endpoints.base + "/authentication/session/new" + Endpoints.apiKeyParam
+            case .login: return Endpoints.base + "/authentication/token/validate_with_login" + Endpoints.apiKeyParam
+            case .webAuth: return "https://www.themoviedb.org/authenticate/" + Auth.requestToken + "?redirect_to=themoviemanager:authenticate"
+            case .logOut: return Endpoints.base + "/authenticate/session" + Endpoints.apiKeyParam
             }
         }
         
@@ -71,6 +79,75 @@ class TMDBClient {
             }
         }
         task.resume()
+    }
+    class func login(userName: String, password: String ,completion: @escaping (Bool, Error?) -> Void){
+        var request = URLRequest(url: Endpoints.login.url)
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        let body = LoginRequest(username: userName, password: password, requestToken: Auth.requestToken)
+        request.httpBody = try!JSONEncoder().encode(body)
+        let task = URLSession.shared.dataTask(with: request){(data,response,error) in
+            guard let data = data else {
+                completion(false, error)
+                return
+            }
+            
+            do{
+                let decoder = JSONDecoder()
+                let responseObject = try decoder.decode(RequestTokenResponse.self, from: data)
+                Auth.requestToken = responseObject.requestTokenRecieved
+                completion(true, nil)
+            }
+            catch{
+                completion(false,error)
+            }
+          
+        }
+         task.resume()
+    }
+    
+    class func logOut (completion:@escaping () -> Void){
+        var request = URLRequest(url: Endpoints.logOut.url)
+        request.httpMethod = "DELETE"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        let body = LogoutRequest(sessionId: Auth.sessionId)
+        request.httpBody = try!JSONEncoder().encode(body)
+        
+        let task = URLSession.shared.dataTask(with: request){data,response,error in
+            Auth.requestToken = ""
+            Auth.sessionId = ""
+            completion()
+            }
+        task.resume()
+    }
+    
+    class func getRequestSessionId(completion: @escaping (Bool,Error?) -> Void){
+       
+        var request = URLRequest(url: Endpoints.createSessionId.url)
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        let body = PostSession(requestToken: Auth.requestToken)
+        request.httpBody  = try!JSONEncoder().encode(body)
+        
+        let task = URLSession.shared.dataTask(with: request) {data,response,error in
+            guard let data = data else{
+                completion(false,error)
+                return
+            }
+            
+            do{
+                let decoder = JSONDecoder()
+                let responseObject = try decoder.decode(SessionResponse.self, from: data)
+                Auth.sessionId = responseObject.sessionId
+                completion(true,nil)
+            }
+            catch{
+                completion(false,error)
+            }
+        }
+        
+        task.resume()
+    
     }
     
 }
